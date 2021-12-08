fb4974dea440548cf49792ccfee7f46d3aa86d79
Guled  Ibrahim <gman_000@yahoo.com>
2021-12-08 23:20:49 +0000

    children filter buttons have been added

--- src/components/FilterDraw/index.js
+++ src/components/FilterDraw/index.js
@@ -124,6 +124,45 @@ const FilterDraw = ({ toggleNavbar, locations }) => {
               </button>
             </div>
           </div>
+          <div className='flex lg:flex lg:flex-col mt-8'>
+            <p className='font-bold capitalize'>children</p>
+            <p className='font-light my-4'>Ages 12-2</p>
+            <div className='lg:flex'>
+              <button>
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  className='h-6 w-6 border-2 border-gray-200 rounded-sm mr-2 text-gray-400'
+                  fill='none'
+                  viewBox='0 0 24 24'
+                  stroke='currentColor'
+                >
+                  <path
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
+                    strokeWidth={2}
+                    d='M20 12H4'
+                  />
+                </svg>
+              </button>
+              <span>0</span>
+              <button>
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  className='h-6 w-6 border-2 border-gray-200 rounded-sm ml-2 text-gray-400'
+                  fill='none'
+                  viewBox='0 0 24 24'
+                  stroke='currentColor'
+                >
+                  <path
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
+                    strokeWidth={2}
+                    d='M12 4v16m8-8H4'
+                  />
+                </svg>
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
