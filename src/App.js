7cfb51653d6703e4cae7abd0b1589995740f7feb
Guled  Ibrahim <gman_000@yahoo.com>
2021-12-14 22:16:00 +0000

    removed filter draw component for a simpler search function

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import { useState } from 'react';
 import Navbar from './components/Navbar';
 import ResultBanner from './components/ResultBanner';
-import FilterDraw from './components/FilterDraw';
 import CardList from './components/CardList';
 import stays from './stays.json';
 
 const App = () => {
   const [data, setData] = useState(stays);
-  const [toggle, istoggle] = useState(true);
-  const [location, setLocation] = useState('');
 
   const filterData = (total, childTotal, adultTotal) => {
-    const totalGuests = childTotal + adultTotal;
+    /* const totalGuests = childTotal + adultTotal;
     total > 0
       ? setData(
           stays.filter(
@@ -24,24 +21,12 @@ const App = () => {
             ({ city, maxGuests }) =>
               city === location && maxGuests === parseInt(totalGuests)
           )
-        );
-
-    istoggle(true);
+        ); */
   };
 
   return (
     <div className='flex flex-col lg:flex lg:justify-between lg:flex-row lg:items-center lg:mt-12 lg:mx-8 lg:flex-wrap'>
-      {toggle ? (
-        <Navbar toggleNavbar={(toggle) => istoggle(toggle)} />
-      ) : (
-        <FilterDraw
-          toggleNavbar={(toggle) => istoggle(toggle)}
-          filterLocationValue={(location) => setLocation(location)}
-          filterData={filterData}
-          locations={['London', 'Birmingham', 'Liverpool', 'Edinburgh']}
-          location={location}
-        />
-      )}
+      <Navbar />
       <ResultBanner results={data.length} />
       <CardList data={data} />
     </div>
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,37 +1,42 @@
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
 import logo from '../../svg/logo.svg';
 
-const Navbar = ({ toggleNavbar }) => {
+const Navbar = () => {
+  const [location, setLocation] = useState('');
+  const [totalGuests, setTotalGuests] = useState();
   return (
     <Fragment>
       <div className='flex w-full lg:w-auto'>
         <img src={logo} alt='logo.svg' className='py-4 px-4' />
       </div>
       <div className='flex justify-center mx-4 lg:w-1/4 lg:mt-0'>
-        <div className='flex w-full shadow-lg rounded-l-2xl rounded-r-2xl mt-8 lg:mt-0'>
+        <div className='flex flex-col w-full mt-8 lg:mt-0 lg:flex-row lg:items-center lg:mr-8'>
           <input
+            value={location}
             type='text'
             placeholder='London'
-            className=' border-2 border-r-0 border-gray=200 rounded-l-2xl py-2 px-2 text-sm w-3/4 focus:outline-none text-center capitalize'
-            onClick={() => toggleNavbar(false)}
+            className='border-2 border-gray-300 py-4 px-2 capitalize focus:outline-none rounded-tr-lg rounded-tl-lg lg:py-2 lg:px-0 lg:rounded-l-lg lg:border-2 lg:border-r-1 lg:rounded-r-none'
+            onChange={({ target }) => setLocation(target.value)}
           />
           <input
+            value={totalGuests}
             type='number'
-            max='10'
             min='0'
             placeholder='add guests'
-            className='border-2 border-r-1 border-gray-200 py-2 px-2 text-sm w-3/4 focus:outline-none h-14 text-center capitalize appearance-none'
+            className='border-2 border-t-0 border-gray-300 py-4 px-2 capitalize focus:outline-none rounded-br-lg rounded-bl-lg lg:py-2 lg:px-0 lg:rounded-none lg:border-2 lg:border-l-0 lg:border-r-1'
+            onChange={({ target }) => setTotalGuests(target.value)}
           />
           <button
             type='submit'
-            className='border-2 border-l-0 border-gray-200 rounded-r-2xl py-2 px-2 text-sm w-1/4 relative lg:bg-white lg:hover:bg-red-200'
+            className='py-2 bg-red-400 relative mt-6 rounded-md flex justify-center items-center w-1/2 self-center lg:mt-0 lg:bg-transparent lg:border-2 lg:border-gray-300 lg:border-l-0 lg:rounded-l-none'
           >
             <svg
               xmlns='http://www.w3.org/2000/svg'
               fill='none'
               viewBox='0 0 24 24'
               stroke='currentColor'
-              className='h-6 w-6 absolute text-red-500 top-4 right-4 lg:h-6 lg:w-6 lg:top-3 lg:right-4'
+              className='h-6 w-6 text-white mr-2 lg:text-red-400'
+              //className='h-6 w-6 absolute text-red-500 top-4 right-4 lg:h-6 lg:w-6 lg:text-center'
             >
               <path
                 strokeLinecap='round'
@@ -40,6 +45,7 @@ const Navbar = ({ toggleNavbar }) => {
                 d='M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z'
               />
             </svg>
+            <span className='text-xl text-white lg:hidden'>search</span>
           </button>
         </div>
       </div>
